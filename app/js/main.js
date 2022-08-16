c2a666001eee3b2296d0c3175908af1109ac6cb7
alexMstar1 <aliaksei.kavalionak@innowise-group.com>
2022-08-16 18:24:33 +0300

    creating new section Sagen in einer neuen Dimension. Adding JS functionality for show/less list

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,60 @@
-import {testFunction} from './scripts/test';
-testFunction();
+(function () {
+  const showListWrapper = document.querySelectorAll(
+    ".cut__show"
+  );
+
+  if (showListWrapper.length) {
+    showListWrapper.forEach(function (wrapper) {
+      const showList = wrapper.querySelector(
+        ".cut__list-show"
+      );
+
+      const showListItems = showList.querySelectorAll(
+        ".cut__var"
+      );
+
+      const showMoreButton = wrapper.querySelector(
+        ".cut__btn"
+      );
+
+      if (
+        showListWrapper &&
+        showListItems.length > 2 &&
+        showMoreButton
+      ) {
+        let isCollapsed = false;
+
+        function toggleItemsVisibility(show) {
+          showListItems.forEach(function (item, index) {
+            if (index < 2) {
+              return;
+            }
+
+            item.style.display = show ? "" : "none";
+          });
+        }
+
+        toggleItemsVisibility(false);
+        showMoreButton.classList.add("__is-visible");
+        isCollapsed = true;
+        showMoreButton.addEventListener("click", function () {
+          if (isCollapsed) {
+            toggleItemsVisibility(true);
+            showMoreButton.innerText = "Weniger Details";
+
+            isCollapsed = false;
+            return;
+          }
+
+          if (!isCollapsed) {
+            toggleItemsVisibility(false);
+            showMoreButton.innerText = "Mehr Details";
+            isCollapsed = true;
+            return;
+          }
+        });
+      }
+    });
+  }
+})();
+
